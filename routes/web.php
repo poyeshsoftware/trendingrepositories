23ad91b375d74c2678b62690d153e8baddbfdc08
Barzan <payamprivate2@gmail.com>
2020-07-26 22:23:53 +0430

    adding required info to README.md file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('/api/repositories/search');
 });
